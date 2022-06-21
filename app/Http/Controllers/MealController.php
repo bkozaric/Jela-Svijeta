7b10683bf0783be30c0d516f9d678ff7a5f611e3
unknown <bernardkozaric@gmail.com>
2022-06-21 15:04:20 +0200

    Filters added

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -9,9 +9,8 @@ use Illuminate\Support\Facades\Validator;
 class MealController extends Controller
 {
     //
-    public function query(Request $req){
-
-
+    public function query(Request $req)
+    {
         $validator = Validator::make($req->all(), [
             'per_page' => 'sometimes|integer',
             'page' => 'sometimes|integer',
@@ -21,16 +20,75 @@ class MealController extends Controller
             'lang' => 'required',
             'diff_time' => 'sometimes|integer',
         ]);
- 
+
         if ($validator->fails()) {
             return "<h2>Incorrect query</h2>";
         }
- 
-        $validated = $validator->validated();
 
-        return $validated['with'];
+        app()->setLocale($req->query('lang'));
+        $query = Meal::query()->withTranslation();
 
-        //return $req->with;
-        //return Meal::all();
+        /*if ($req->has('category')) {
+            $categoryId = $req->query('category');
+
+            if ($categoryId == (string)(int)$categoryId) {
+
+                $query = $query->where('category_id', $categoryId);
+            } elseif ($categoryId == 'null') {
+
+                $query = $query->whereNull('category_id');
+            } elseif ($categoryId == '!null') {
+
+                $query = $query->whereNotNull('category_id');
+            }
+        }*/
+
+
+        if ($req->has('tags')) {
+            $tags = explode(',', $req->query('tags'));
+
+            foreach ($tags as $tag) {
+                $query = $query->whereHas('tags', function ($q) use ($tag) {
+                    $q->where('tags.id', '=', $tag);
+                });
+            }
+        }
+
+        
+        if ($req->has('with')) {
+
+            $with = explode(',', $req->query('with'));
+
+    		$props = array(
+				'ingredients',
+				'category',
+				'tags'
+			);
+		
+			$list = array_intersect($props, $with);
+
+			$query = $query->with($list);
+        }
+        
+        if ($req->has('diff_time')) {
+            $diffDate = new \DateTime();
+		    $diffDate->setTimestamp($req->query('diff_time'));
+
+            $query = $query->withTrashed()
+                    ->where('created_at','>',$diffDate)
+                    ->orWhere('updated_at','>',$diffDate)
+                    ->orWhere('deleted_at','>',$diffDate);
+            
+        }
+
+    	/*
+    	if($request->has('per_page')){
+
+
+    	}*/
+        
+
+
+        return response()->json($query);
     }
 }
